b292ebddc3dddaaf60d143c97716263c484e604f
Liang Geng <geng.161@osu.edu>
2022-02-26 17:35:17 -0500

    update test program

--- src/core/lib/rdma/test/RDMAClient.cc
+++ src/core/lib/rdma/test/RDMAClient.cc
@@ -1,217 +1,197 @@
-#include "../RDMASenderReceiver.h"
-#include "Utils.h"
-#include <unistd.h>
-#include <thread>
-#include "netinet/tcp.h"
-#include "SockUtils.h"
+#include <fcntl.h>
 #include <sys/socket.h>
 #include <sys/stat.h>
-#include <fcntl.h>
+#include <unistd.h>
+#include <chrono>
 #include <condition_variable>
 #include <mutex>
-#include <chrono>
-
+#include <queue>
+#include <thread>
+#include "../RDMASenderReceiver.h"
+#include "SockUtils.h"
+#include "netinet/tcp.h"
+int random(int min, int max) {
+  static bool first = true;
+  if (first) {
+    srand(time(NULL));  // seeding for the first time only!
+    first = false;
+  }
+  return min + rand() % ((max + 1) - min);
+}
 
 class RDMAClient {
-  public:
-    RDMAClient() {
-      sockfd_ = SocketUtils::socket(AF_INET, SOCK_STREAM, 0);
+ public:
+  RDMAClient() { sockfd_ = SocketUtils::socket(AF_INET, SOCK_STREAM, 0); }
+
+  int connect(const char* server_address, int port) {
+    server_address_ = server_address_;
+    port_ = port;
+    struct sockaddr_in serv_addr;
+    char serv_ip[16];
+    strcpy(serv_ip, server_address);
+    bzero(&serv_addr, sizeof(serv_addr));
+    SocketUtils::setAddr(serv_addr, serv_ip, port);
+
+    int flag = 1;
+    if (setsockopt(sockfd_, IPPROTO_TCP, TCP_NODELAY, (char*)&flag,
+                   sizeof(flag))) {
+      rdma_log(RDMA_ERROR,
+               "RDMAClient::connect, error on setsockopt (TCP_NODELAY)");
+      exit(-1);
     }
-
-    int connect(const char* server_address, int port) {
-      server_address_ = server_address_;
-      port_ = port;
-      struct sockaddr_in serv_addr;
-      char serv_ip[16];
-      strcpy(serv_ip, server_address);
-      bzero(&serv_addr, sizeof(serv_addr));
-      SocketUtils::setAddr(serv_addr, serv_ip, port);
-
-      int flag = 1;
-      if (setsockopt(sockfd_, IPPROTO_TCP, TCP_NODELAY, (char *)&flag, sizeof(flag))) {
-        rdma_log(RDMA_ERROR, "RDMAClient::connect, error on setsockopt (TCP_NODELAY)");
-        exit(-1);
-      }
-      printf("client connect to %s:%d\n", server_address, port);
-      if (::connect(sockfd_, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
-        rdma_log(RDMA_ERROR, "RDMAClient::connect, error on connect");
-        exit(-1);
-      }
-      return sockfd_;
+    printf("client connect to %s:%d\n", server_address, port);
+    if (::connect(sockfd_, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) <
+        0) {
+      rdma_log(RDMA_ERROR, "RDMAClient::connect, error on connect");
+      exit(-1);
     }
+    return sockfd_;
+  }
 
-  private:
-    int sockfd_;
-    char* server_address_;
-    int port_;
+ private:
+  int sockfd_;
+  char* server_address_;
+  int port_;
 };
 
-#define SEND_BUF_SZ (1024 * 1024 * 6)
-#define BATCH_SZ (1000000)
-
-static long long total_send_size = 0;
-static size_t send_id = 0;
-static size_t send_timeout_s = 10, recv_timeout_s = 10;
-std::condition_variable send_timer, recv_timer;
-std::mutex send_mtx, recv_mtx;
-bool send_timer_start = false, sender_stop = false, sender_alive = true;
-bool recv_timer_start = false, recver_stop = false, recver_alive = true;
-size_t send_size;
-
-static void send_diagnosis(RDMASenderReceiverBP* rdmasr) {
-  std::unique_lock<std::mutex> lck(send_mtx);
-
-  while (sender_alive) {
-    while (!send_timer_start) { std::this_thread::yield(); }
-
-    if (send_timer.wait_for(lck, std::chrono::seconds(send_timeout_s)) == std::cv_status::no_timeout) {
-      continue;
-    }
-
-    // timeout
-    sender_stop = true;
-    printf("\n\nsender stopped, send size = %d\n", send_size);
-    // while (sender_stop) {
-    //   std::this_thread::yield();
-    // }
-    rdmasr->diagnosis();
-    sleep(10);
-    while (sender_stop) {}
-  }
-}
-
-static void recv_diagnosis(RDMASenderReceiverBP* rdmasr) {
-  std::unique_lock<std::mutex> lck(recv_mtx);
-
-  while (recver_alive) {
-    while (!recv_timer_start) { std::this_thread::yield(); }
-
-    if (recv_timer.wait_for(lck, std::chrono::seconds(recv_timeout_s)) == std::cv_status::no_timeout) {
-      continue;
-    }
-
-    // timeout
-    recver_stop = true;
-    printf("\n\nrecver stopped\n");
-    // printf("recver stopped\n");
-    // while (recver_stop) {
-    //   std::this_thread::yield();
-    // }
-    rdmasr->diagnosis();
-    sleep(10);
-    while (recver_stop) {}
-  }
-}
-
-
-void send_thread_bp(RDMASenderReceiverBP* rdmasr) {
-  uint8_t *send_data = new uint8_t[SEND_BUF_SZ];
-  // read(open("/dev/random", O_RDONLY), send_data, SEND_BUF_SZ);
-  for (size_t i = 0; i < SEND_BUF_SZ; i++) send_data[i] = (i % 254) + 2;
-  // memset(send_data, 10, SEND_BUF_SZ);
-  size_t send_data_sz;
-  struct msghdr send_msg;
-  struct iovec send_iov;
-  send_iov.iov_base = send_data;
-  send_msg.msg_iov = &send_iov;
-  send_msg.msg_iovlen = 1;
-
-  std::thread diagnosis(send_diagnosis, rdmasr);
-
-  // long long total_sent_sz = 0;
-  for (int i = 0; i < BATCH_SZ; i++) {
-    send_data_sz = random(1, SEND_BUF_SZ);
-    send_iov.iov_len = send_data_sz;
-
-    send_size = send_data_sz;
-    send_timer_start = true;
-    while (rdmasr->send(&send_msg, send_data_sz) == false) {
-      while(sender_stop) { 
-        std::this_thread::yield(); 
+struct LastN {
+  using T = size_t;
+  void add(T e) {
+    std::lock_guard<std::mutex> lg(mutex_);
+    if (buf_.size() == limit) {
+      for (int i = 0; i < limit - 1; i++) {
+        buf_[i] = buf_[i + 1];
       }
+      buf_[limit - 1] = e;
+    } else {
+      buf_.push_back(e);
     }
-    send_timer_start = false;
-    send_timer.notify_one();
-
-    total_send_size += send_data_sz;
-    // printf("%d-th send %d bytes, total sent size = %lld\n", i, send_data_sz, total_sent_sz);
-    send_id++;
   }
-  printf("send complete\n");
 
-  sender_alive = false;
-  diagnosis.join();
-  delete send_data;
-}
+  void print() {
+    std::lock_guard<std::mutex> lg(mutex_);
+    for (int i = 0; i < buf_.size(); i++) {
+      printf("%2zu ", buf_[i]);
+    }
+    printf("\n");
+  }
+  size_t limit = 10;
+  std::vector<T> buf_;
+  std::mutex mutex_;
+};
 
-int main(int argc, char *argv[]) {
+#define SEND_BUF_SZ (80)
 
+int main(int argc, char* argv[]) {
+  if (argc != 2) {
+    printf("Bad param, need ip addr\n");
+    exit(1);
+  }
   RDMAClient client;
-  int fd = client.connect("10.3.1.6", 50050);
+  int fd = client.connect(argv[1], 50050);
 
   RDMASenderReceiverBP rdmasr;
   rdmasr.connect(fd);
   printf("connection established\n");
 
-  std::thread send_thread(send_thread_bp, &rdmasr);
-
   struct msghdr recv_msg;
   struct iovec recv_iov;
+  std::mutex mutex;
+  std::queue<std::vector<char>> sent_data;
   recv_msg.msg_iov = &recv_iov;
   recv_msg.msg_iovlen = 1;
-  uint8_t *recv_buf = new uint8_t[1024 * 1024 * 16];
-  size_t batch_sz = BATCH_SZ;
-  size_t send_buf_sz = SEND_BUF_SZ;
+  std::atomic_int sent_count;
+  sent_count.store(0);
 
-  long long check_point = send_buf_sz * 100;
-  long long check_point_inc = check_point;
+  LastN lastN, lastNrecv;
 
-  size_t id = 0;
-  long long total_recv_sz = 0;
-  long long target_recv_sz = batch_sz * send_buf_sz;
+  std::thread th_recv = std::thread([&]() {
+    size_t recv_count = 0;
+    while (true) {
+      auto msg_size = rdmasr.check_and_ack_incomings();
 
-  std::condition_variable timer;
-  std::mutex mtx;
-  std::atomic<bool> started(false), stop(false);
+      if (msg_size > 0) {
+        if (msg_size > SEND_BUF_SZ) {
+          printf("Unexpected len: %zu vs %d", msg_size, SEND_BUF_SZ);
+          assert(msg_size <= SEND_BUF_SZ);
+        }
+        std::vector<char> data_to_recv(msg_size);
+        std::vector<char> should_recv;
+        struct msghdr msg;
+        struct iovec iov;
 
-  std::thread diagnosis(recv_diagnosis, &rdmasr);
+        msg.msg_iov = &iov;
+        msg.msg_iovlen = 1;
+        iov.iov_base = data_to_recv.data();
+        iov.iov_len = msg_size;
+        recv_count++;
+        lastNrecv.add(msg_size);
+
+        {
+          std::lock_guard<std::mutex> lg(mutex);
+          if (sent_data.empty()) {
+            printf("Queue is empty, msg_len: %zu sent cnt: %d recv cnt: %zu\n",
+                   msg_size, sent_count.load(), recv_count);
+            printf("Sender len\n");
+            lastN.print();
+            printf("Recv len\n");
+            lastNrecv.print();
+            assert(!sent_data.empty());
+          }
+          should_recv = sent_data.front();
+          assert(should_recv.size() == msg_size);
+          sent_data.pop();
+        }
+
+        auto read_size = rdmasr.recv(&msg);
+        if (read_size != msg_size) {
+          printf("%zu vs %zu\n", read_size, msg_size);
+          assert(read_size != msg_size);
+        }
+
+        if (should_recv != data_to_recv) {
+          printf("Recv wrong data, got\n");
+          for (auto& e : data_to_recv) {
+            printf("%2d ", e);
+          }
+
+          printf("\nShould be: \n");
+          for (auto& e : should_recv) {
+            printf("%2d ", e);
+          }
+          printf("\n");
+          exit(1);
+        }
+      }
+    }
+  });
+  th_recv.detach();
+
   while (true) {
+    auto send_data_sz = random(1, SEND_BUF_SZ);
+    std::vector<char> data_to_send(send_data_sz);
 
-    recv_timer_start = true;
-    while (rdmasr.check_incoming() == false || rdmasr.check_and_ack_incomings() == 0) {
-      while(stop) { std::this_thread::yield(); }
-    }
-    recv_timer_start = false;
-    recv_timer.notify_one();
-
-    int check_size = rdmasr.check_and_ack_incomings();
-    recv_iov.iov_base = recv_buf;
-    recv_iov.iov_len = check_size;
-    // printf("\t %d-th data incoming %d\n", id, check_size);
-    int read_size = rdmasr.recv(&recv_msg);
-    total_recv_sz += read_size;
-    // printf("\t %d-th recv %d bytes, total recv size = %lld\n", 
-    //       id, read_size, total_recv_sz);
-
-    if (id % 1000 == 0) {
-      printf("%d-th, total recv %lld bytes, total send (%d times, %lld bytes)\n", 
-              id, total_recv_sz, send_id, total_send_size);
-      check_point += check_point_inc;
+    for (int i = 0; i < data_to_send.size(); i++) {
+      data_to_send[i] = i % 254 + 2;
     }
 
-    if (total_recv_sz == total_send_size && send_id == BATCH_SZ) {
-      printf("recv complete, total recv %lld size, id = %d, total send %d times\n", 
-              total_recv_sz, id, send_id);
-      break;
+    struct msghdr send_msg;
+    struct iovec send_iov;
+    send_iov.iov_base = data_to_send.data();
+    send_iov.iov_len = send_data_sz;
+    send_msg.msg_iov = &send_iov;
+    send_msg.msg_iovlen = 1;
+
+    printf("Send: %d\n", send_data_sz);
+    while (!rdmasr.send(&send_msg, send_data_sz))
+      ;
+    {
+      std::lock_guard<std::mutex> lg(mutex);
+      sent_data.push(data_to_send);
     }
-    id++;
+    sent_count++;
+    lastN.add(send_data_sz);
   }
-
-  recver_alive = false;
-  delete recv_buf;
-
-
-  send_thread.join();
 
   return 0;
 }
--- src/core/lib/rdma/test/RDMAServer.cc
+++ src/core/lib/rdma/test/RDMAServer.cc
@@ -1,18 +1,17 @@
-#include "../RDMASenderReceiver.h"
-#include "Utils.h"
-#include "SockUtils.h"
-#include "thpool.h"
 #include <netinet/tcp.h>
 #include <sys/socket.h>
+#include <chrono>
 #include <condition_variable>
 #include <mutex>
-#include <chrono>
 #include <thread>
+#include "../RDMASenderReceiver.h"
+#include "SockUtils.h"
+#include "thpool.h"
 
 #define MAX_CONN_NUM 10
 #define MAX_THREAD_NUM 10
 #define DEFAULT_PORT 50050
-#define MAX_BUF_SZ  (1024 * 1024 * 8)
+#define MAX_BUF_SZ (1024 * 1024 * 8)
 
 class RDMAServer;
 
@@ -24,187 +23,155 @@ typedef struct thread_handler_args {
 typedef void* (*thread_handler)(void* args);
 
 class RDMAServer {
-  public:
-    RDMAServer() {
-      sockfd_ = SocketUtils::socket(AF_INET, SOCK_STREAM, 0);
-      int opt = 1;
-      if (setsockopt(sockfd_, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt))) {
-          rdma_log(RDMA_ERROR, "RDMAServer::RDMAServer, error on setsockopt (SO_REUSEADDR)");
-          exit(-1);
-      }
-      opt = 1;
-      if (setsockopt(sockfd_, SOL_SOCKET, SO_REUSEPORT, &opt, sizeof(opt))) {
-          rdma_log(RDMA_ERROR, "RDMAServer::RDMAServer, error on setsockopt (SO_REUSEPORT)");
-          exit(-1);
-      }
+ public:
+  RDMAServer() {
+    sockfd_ = SocketUtils::socket(AF_INET, SOCK_STREAM, 0);
+    int opt = 1;
+    if (setsockopt(sockfd_, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt))) {
+      rdma_log(RDMA_ERROR,
+               "RDMAServer::RDMAServer, error on setsockopt (SO_REUSEADDR)");
+      exit(-1);
+    }
+    opt = 1;
+    if (setsockopt(sockfd_, SOL_SOCKET, SO_REUSEPORT, &opt, sizeof(opt))) {
+      rdma_log(RDMA_ERROR,
+               "RDMAServer::RDMAServer, error on setsockopt (SO_REUSEPORT)");
+      exit(-1);
+    }
 
-      pool_ = thpool_init(MAX_THREAD_NUM);
-      if (!pool_) {
-        rdma_log(RDMA_ERROR, "RDMAServer::RDMAServer, error on thpool_init");
+    pool_ = thpool_init(MAX_THREAD_NUM);
+    if (!pool_) {
+      rdma_log(RDMA_ERROR, "RDMAServer::RDMAServer, error on thpool_init");
+      exit(-1);
+    }
+  }
+
+  void attach(thread_handler handler) { handler_ = handler; }
+
+  void start(int port) {
+    port_ = port;
+    struct sockaddr_in server_addr;
+    bzero(&server_addr, sizeof(struct sockaddr_in));
+    SocketUtils::setAddr(server_addr, port);
+    if (::bind(sockfd_, (struct sockaddr*)&server_addr, sizeof(server_addr)) <
+        0) {
+      rdma_log(RDMA_ERROR, "RDMAServer::start, error on bind");
+      exit(-1);
+    }
+    if (::listen(sockfd_, MAX_CONN_NUM) < 0) {
+      rdma_log(RDMA_ERROR, "RDMAServer::start, error on listen");
+      exit(-1);
+    }
+
+    struct sockaddr client_sockaddr;
+    socklen_t addr_len = sizeof(client_sockaddr);
+    char addr_buf[1024];
+    const char* addr_buf_ptr;
+    int client_port, id = 0;
+    while (true) {
+      int newsd = accept(sockfd_, &client_sockaddr, &addr_len);
+      if (newsd < 0) {
+        rdma_log(RDMA_ERROR, "RDMAServer::start, error on accept");
         exit(-1);
       }
+      switch (client_sockaddr.sa_family) {
+        case AF_INET:
+          addr_buf_ptr = inet_ntop(
+              AF_INET, &(((struct sockaddr_in*)&client_sockaddr)->sin_addr),
+              addr_buf, 1024);
+          client_port = ((struct sockaddr_in*)&client_sockaddr)->sin_port;
+          break;
+        case AF_INET6:
+          addr_buf_ptr = inet_ntop(
+              AF_INET6, &(((struct sockaddr_in6*)&client_sockaddr)->sin6_addr),
+              addr_buf, 1024);
+          client_port = ((struct sockaddr_in6*)&client_sockaddr)->sin6_port;
+          break;
+      }
+      // rdma_log(RDMA_INFO, "TCPServer::start, accept connection from %s:%d
+      // newsd = %d",
+      //                      addr_buf_ptr, client_port, newsd);
+
+      printf("TCPServer::start, accept connection from %s:%d newsd = %d\n",
+             addr_buf_ptr, client_port, newsd);
+      int flag = 1;
+      setsockopt(newsd, IPPROTO_TCP, TCP_NODELAY, &flag, sizeof(flag));
+      thread_handler_args* hargs =
+          (thread_handler_args*)malloc(sizeof(thread_handler_args));
+      hargs->sockfd = newsd;
+      hargs->id = id++;
+      thpool_add_work(pool_, handler_, (void*)hargs);
     }
+  }
 
-    void attach(thread_handler handler) { handler_ = handler; }
-
-    void start(int port) {
-      port_ = port;
-      struct sockaddr_in server_addr;
-      bzero(&server_addr, sizeof(struct sockaddr_in));
-      SocketUtils::setAddr(server_addr, port);
-      if (::bind(sockfd_, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
-          rdma_log(RDMA_ERROR, "RDMAServer::start, error on bind");
-          exit(-1);
-      }
-      if (::listen(sockfd_, MAX_CONN_NUM) < 0) {
-          rdma_log(RDMA_ERROR, "RDMAServer::start, error on listen");
-          exit(-1);
-      }
-
-      struct sockaddr client_sockaddr;
-      socklen_t addr_len = sizeof(client_sockaddr);
-      char addr_buf[1024];
-      const char *addr_buf_ptr;
-      int client_port, id = 0;
-      while (true) {
-        int newsd = accept(sockfd_, &client_sockaddr, &addr_len);
-        if (newsd < 0) {
-          rdma_log(RDMA_ERROR, "RDMAServer::start, error on accept");
-          exit(-1);
-        }
-        switch (client_sockaddr.sa_family) {
-          case AF_INET:
-            addr_buf_ptr = inet_ntop(AF_INET, &(((struct sockaddr_in *)&client_sockaddr)->sin_addr), addr_buf, 1024);
-            client_port = ((struct sockaddr_in *)&client_sockaddr)->sin_port;
-            break;
-          case AF_INET6:
-            addr_buf_ptr = inet_ntop(AF_INET6, &(((struct sockaddr_in6 *)&client_sockaddr)->sin6_addr), addr_buf, 1024);
-            client_port = ((struct sockaddr_in6 *)&client_sockaddr)->sin6_port;
-            break;
-        }
-        // rdma_log(RDMA_INFO, "TCPServer::start, accept connection from %s:%d newsd = %d",
-        //                      addr_buf_ptr, client_port, newsd);
-
-        printf("TCPServer::start, accept connection from %s:%d newsd = %d\n",
-                             addr_buf_ptr, client_port, newsd);
-        int flag = 1;
-        setsockopt(newsd, IPPROTO_TCP, TCP_NODELAY, &flag, sizeof(flag));
-        thread_handler_args *hargs = (thread_handler_args *)malloc(sizeof(thread_handler_args));
-        hargs->sockfd = newsd;
-        hargs->id = id++;
-        thpool_add_work(pool_, handler_, (void*)hargs);
-      }
-    }
-
-  private:
-    int sockfd_;
-    int port_;
-    threadpool pool_;
-    thread_handler handler_;
+ private:
+  int sockfd_;
+  int port_;
+  threadpool pool_;
+  thread_handler handler_;
 };
 
-static size_t send_recv_timeout_s = 10;
-std::condition_variable send_recv_timer;
-std::mutex send_recv_mtx;
-bool send_recv_timer_start = false, sender_recver_stop = false, sender_recver_alive = true;
-int send1recv2=0;
-size_t send_size;
-
-static void send_recv_diagnosis(RDMASenderReceiverBP* rdmasr) {
-  std::unique_lock<std::mutex> lck(send_recv_mtx);
-
-  while (sender_recver_alive) {
-    while (!send_recv_timer_start) { std::this_thread::yield(); }
-
-    if (send_recv_timer.wait_for(lck, std::chrono::seconds(send_recv_timeout_s)) == std::cv_status::no_timeout) {
-      continue;
-    }
-
-    // timeout
-    sender_recver_stop = true;
-    switch (send1recv2) {
-      case 1:
-        printf("\n\nsender stopped, send size = %d\n", send_size);
-        break;
-      case 2:
-        printf("\n\nrecver stopped\n");
-        break;
-    }
-    // printf("sender or recver stopped\n");
-    // while (sender_recver_stop) {
-    //   std::this_thread::yield();
-    // }
-    rdmasr->diagnosis();
-    while (sender_recver_stop) {}
-  }
-}
-
 static void* thread_handler_bp(void* args) {
-      thread_handler_args *hargs = (thread_handler_args *)args;
-      int sockfd= hargs->sockfd;
-      RDMASenderReceiverBP* rdmasr = new RDMASenderReceiverBP();
-      rdmasr->connect(sockfd);
-      printf("%d-th connection established\n", hargs->id);
-      // uint8_t *recv_buf = new uint8_t[MAX_BUF_SZ];
+  thread_handler_args* hargs = (thread_handler_args*)args;
+  int sockfd = hargs->sockfd;
+  RDMASenderReceiverBP* rdmasr = new RDMASenderReceiverBP();
+
+  rdmasr->connect(sockfd);
+  printf("%d-th connection established\n", hargs->id);
+  uint8_t* recv_buf = new uint8_t[1024 * 1024 * 16];
+
+  std::atomic_bool updated;
+  updated.store(false);
+  std::thread th_monitor = std::thread([&]() {
+    while (true) {
+      sleep(2);
+      if (!updated.load()) {
+        printf("Stuck\n");
+        exit(1);
+      } else {
+        updated.store(false);
+      }
+    }
+  });
+
+  while (true) {
+    auto msg_size = rdmasr->check_and_ack_incomings();
+
+    if (msg_size > 0) {
       struct msghdr msg;
       struct iovec iov;
+
       msg.msg_iov = &iov;
       msg.msg_iovlen = 1;
-      uint8_t *recv_buf = new uint8_t[1024 * 1024 * 16];
-
-      std::thread diagnosis(send_recv_diagnosis, rdmasr);
-
-      size_t id = 0;
-      while (true) {
-
-        send1recv2 = 2;
-        send_recv_timer_start = true;
-        while (rdmasr->check_and_ack_incomings() == 0) {
-          while(sender_recver_stop) { 
-            std::this_thread::yield(); 
-          }
-        }
-        send_recv_timer_start = false;
-        send_recv_timer.notify_one();
-
-        int check_size = rdmasr->check_and_ack_incomings();
-        iov.iov_base = recv_buf;
-        iov.iov_len = check_size;
-        // printf("%d-th data incoming %d\n", id, check_size);
-        int read_size = rdmasr->recv(&msg);
-
-        int send_length = read_size;
-        while (send_length > 0) {
-          size_t n = MIN(send_length, rdmasr->get_max_send_size());
-          send_length -= n;
-          iov.iov_len = n;
-
-          send1recv2 = 1;
-          send_size = n;
-          send_recv_timer_start = true;
-          while (rdmasr->send(&msg, n) == false) {
-            while(sender_recver_stop) { 
-              std::this_thread::yield(); 
-            }
-          }
-          send_recv_timer_start = false;
-          send_recv_timer.notify_one();
-
-          // printf("%d-th send %d bytes\n", id, n);
-          iov.iov_base = (uint8_t*)(iov.iov_base) + n;
-        }
-        // printf("%d-th send %d bytes\n\n", id, read_size);
-
-        id++;
+      iov.iov_base = recv_buf;
+      iov.iov_len = msg_size;
+      if (msg_size >= DEFAULT_RINGBUF_SZ) {
+        printf("wrong len: %zu\n", msg_size);
       }
+      auto read_size = rdmasr->recv(&msg);
 
-      sender_recver_alive = false;
-      diagnosis.join();
-      delete recv_buf;
-      return nullptr;
+      if (read_size != msg_size) {
+        printf("%zu vs %zu\n", read_size, msg_size);
+        exit(1);
+      }
+      struct msghdr send_msg;
+      struct iovec send_iov;
+      send_iov.iov_base = recv_buf;
+      send_iov.iov_len = msg_size;
+      send_msg.msg_iov = &send_iov;
+      send_msg.msg_iovlen = 1;
+
+      while (rdmasr->send(&send_msg, msg_size) == false) {
+      }
+      updated.store(true);
     }
+  }
 
-int main(int argc, char *argv[]) {
+  delete[] recv_buf;
+  return nullptr;
+}
+
+int main(int argc, char* argv[]) {
   RDMAServer server;
   server.attach(thread_handler_bp);
   server.start(DEFAULT_PORT);
