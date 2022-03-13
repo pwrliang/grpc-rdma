a828d412058acd3671482303627385026b557a37
Liang Geng <geng.161@osu.edu>
2022-03-13 19:50:43 -0400

    debug

--- src/core/lib/rdma/RDMASenderReceiverBP.cc
+++ src/core/lib/rdma/RDMASenderReceiverBP.cc
@@ -1,7 +1,7 @@
-#include "RDMASenderReceiver.h"
-#include "log.h"
-#include "fcntl.h"
 #include <thread>
+#include "RDMASenderReceiver.h"
+#include "fcntl.h"
+#include "log.h"
 
 // -----< RDMASenderReceiverBP >-----
 
@@ -11,8 +11,8 @@ RDMASenderReceiverBP::RDMASenderReceiverBP() {
   ringbuf_bp_ = new RingBufferBP(ringbuf_sz_);
   if (local_ringbuf_mr_.local_reg(pd, ringbuf_bp_->get_buf(), ringbuf_sz_)) {
     rdma_log(RDMA_ERROR,
-              "RDMASenderReceiverBP::RDMASenderReceiverBP, failed to local_reg "
-              "local_ringbuf_mr");
+             "RDMASenderReceiverBP::RDMASenderReceiverBP, failed to local_reg "
+             "local_ringbuf_mr");
     exit(-1);
   }
 
@@ -39,22 +39,21 @@ void RDMASenderReceiverBP::connect(int fd) {
 }
 
 bool RDMASenderReceiverBP::check_incoming() {
-
-  // previous checked_ is true (new incoming data already found by another thread), 
-  // keep checked_ true, 
-  // return false, avoid call fd_become_readable twice for the same incoming data
+  // previous checked_ is true (new incoming data already found by another
+  // thread), keep checked_ true, return false, avoid call fd_become_readable
+  // twice for the same incoming data
   if (checked_.exchange(true)) return false;
-  // previous checked_ is false (no incoming data found before), 
+  // previous checked_ is false (no incoming data found before),
   // set checked_ true to prevent other thread entering
 
-  // found new incoming data, 
-  // return true, so fd_become_readable will be called, then RDMASenderReceiverBP::recv will be called 
-  // leave checked_ true. 
-  // after recv finished, set checked_ to false.
-  // if (ringbuf_bp_->check_head()) return true;
+  // found new incoming data,
+  // return true, so fd_become_readable will be called, then
+  // RDMASenderReceiverBP::recv will be called leave checked_ true. after recv
+  // finished, set checked_ to false. if (ringbuf_bp_->check_head()) return
+  // true;
   if (ringbuf_bp_->check_mlen()) return true;
 
-  // no new incoming data found, return false, 
+  // no new incoming data found, return false,
   // leave checked_ false, so other thread could check again
   return !checked_.exchange(false);
 }
@@ -66,11 +65,11 @@ size_t RDMASenderReceiverBP::check_and_ack_incomings_locked() {
 }
 
 size_t RDMASenderReceiverBP::recv(msghdr* msg, size_t msghdr_size) {
-
   // the actual read size is unread_data_size
   size_t mlens = unread_mlens_;
 
-  // since we may read more data than unread_mlens_, mlens will be updated to the real mlens we have read
+  // since we may read more data than unread_mlens_, mlens will be updated to
+  // the real mlens we have read
   size_t lens = ringbuf_bp_->read_to_msghdr(msg, msghdr_size, mlens);
   if (lens == 0) {
     rdma_log(RDMA_WARNING, "RDMASenderReceiverBP::recv, lens == 0");
@@ -81,10 +80,10 @@ size_t RDMASenderReceiverBP::recv(msghdr* msg, size_t msghdr_size) {
   unread_mlens_ = 0;
   garbage_ += lens;
   total_recv_sz += lens;
-//  if (garbage_ >= ringbuf_sz_ / 2) { //garbage_ >= ringbuf_sz_ / 2
+  if (garbage_ >= ringbuf_sz_ / 2) {  // garbage_ >= ringbuf_sz_ / 2
     update_remote_metadata();
     garbage_ = 0;
-//  }
+  }
 
   return mlens;
 }
@@ -102,14 +101,19 @@ bool RDMASenderReceiverBP::send(msghdr* msg, size_t mlen) {
   size_t len = mlen + sizeof(size_t) + 1;
 
   update_local_metadata();
-  size_t used = (remote_ringbuf_sz + remote_ringbuf_tail_ - remote_ringbuf_head_) % remote_ringbuf_sz;
-  if (used + len >= remote_ringbuf_sz - sizeof(size_t) - 1) return false;
+  size_t used =
+      (remote_ringbuf_sz + remote_ringbuf_tail_ - remote_ringbuf_head_) %
+      remote_ringbuf_sz;
+  if (used + len >= remote_ringbuf_sz - sizeof(size_t) - 1) {
+    printf("used: %zu len: %zu used+len: %zu vs %zu\n", used, len, used + len,
+           remote_ringbuf_sz - sizeof(size_t) - 1);
+    return false;
+  }
 
   *(size_t*)sendbuf_ = mlen;
   uint8_t* start = sendbuf_ + sizeof(size_t);
   size_t iov_idx, nwritten;
-  for (iov_idx = 0, nwritten = 0;
-       iov_idx < msg->msg_iovlen && nwritten < mlen;
+  for (iov_idx = 0, nwritten = 0; iov_idx < msg->msg_iovlen && nwritten < mlen;
        iov_idx++) {
     void* iov_base = msg->msg_iov[iov_idx].iov_base;
     size_t iov_len = msg->msg_iov[iov_idx].iov_len;
@@ -117,7 +121,9 @@ bool RDMASenderReceiverBP::send(msghdr* msg, size_t mlen) {
     if (nwritten <= sendbuf_sz_) {
       memcpy(start, iov_base, iov_len);
     } else {
-      rdma_log(RDMA_ERROR, "RDMASenderReceiverBP::send, nwritten = %d, sendbuf size = %d", nwritten, sendbuf_sz_);
+      rdma_log(RDMA_ERROR,
+               "RDMASenderReceiverBP::send, nwritten = %d, sendbuf size = %d",
+               nwritten, sendbuf_sz_);
       return false;
     }
     start += iov_len;
@@ -126,13 +132,16 @@ bool RDMASenderReceiverBP::send(msghdr* msg, size_t mlen) {
   sendbuf_[len - 1] = 1;
 
   if (iov_idx != msg->msg_iovlen || nwritten != mlen) {
-    rdma_log(RDMA_ERROR, "RDMASenderReceiverBP::send, iov_idx = %d, msg_iovlen = %d, nwritten = %d, mlen = %d",
-              iov_idx, msg->msg_iovlen, nwritten , mlen);
+    rdma_log(RDMA_ERROR,
+             "RDMASenderReceiverBP::send, iov_idx = %d, msg_iovlen = %d, "
+             "nwritten = %d, mlen = %d",
+             iov_idx, msg->msg_iovlen, nwritten, mlen);
     exit(-1);
   }
 
-  conn_data_bp_->post_send_and_poll_completion(remote_ringbuf_mr_, remote_ringbuf_tail_,
-                                          sendbuf_mr_, 0, len, IBV_WR_RDMA_WRITE, false);
+  conn_data_bp_->post_send_and_poll_completion(
+      remote_ringbuf_mr_, remote_ringbuf_tail_, sendbuf_mr_, 0, len,
+      IBV_WR_RDMA_WRITE, false);
   remote_ringbuf_tail_ = (remote_ringbuf_tail_ + len) % remote_ringbuf_sz;
   total_send_sz += len;
   return true;
