c6267754cf67a77099105a4390d80dd3fd4dd408
MengJingsong <mengjs.92@gmail.com>
2022-07-22 12:20:39 -0400

    bug fix, RDMA_EVENT passed greeter_cs

--- examples/cpp/helloworld/run_cs.sh
+++ examples/cpp/helloworld/run_cs.sh
@@ -1,11 +1,11 @@
 export GRPC_RDMA_ZEROCOPY_ENABLE=true
 export GRPC_VERBOSITY=INFO
 #export GRPC_TRACE="rdma_sr_bpev,transport_bpev"
-#export GRPC_TRACE="rdma,rdma_sr_event"
+# export GRPC_TRACE="rdma,rdma_sr_event"
 export GRPC_RDMA_AFFINITY=false
 export GRPC_RDMA_BPEV_POLLER=both
 #export GRPC_RDMA_BPEV_POLLING_TIMEOUT=100
-export GRPC_PLATFORM_TYPE=RDMA_BP
+export GRPC_PLATFORM_TYPE=RDMA_EVENT
 export GRPC_RDMA_RING_BUFFER_SIZE=$((1024*1024*64))
 export GRPC_RDMA_BPEV_POLLING_THREAD=2
-./cmake/build/greeter_cs -host="$1" -req $((1024*1024*32)) -resp $((1024*1024*32)) -batch 1000 -threads 8 -worker_id="$2"
+./cmake/build/greeter_cs -host="$1" -req $((1024*1024*4)) -resp $((1024*1024*4)) -batch 1000 -threads 8 -worker_id="$2"
