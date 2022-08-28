31d7547c8292443884bb9d2942db01b0d1d39def
Liang Geng <geng.161@osu.edu>
2022-08-28 14:35:17 -0400

    fix

--- src/core/lib/rdma/rdma_sender_receiver_event.cc
+++ src/core/lib/rdma/rdma_sender_receiver_event.cc
@@ -94,7 +94,7 @@ int RDMASenderReceiverEvent::Send(msghdr* msg, ssize_t* sz) {
     return EINVAL;
   }
 
-  pollLastSendCompletion();
+  PollLastSendCompletion();
 
   if (!isWritable(mlen)) {
     if (GRPC_TRACE_FLAG_ENABLED(grpc_rdma_sr_event_trace)) {
