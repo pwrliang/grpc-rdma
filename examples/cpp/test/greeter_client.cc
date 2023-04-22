a08cd9e6c50e4c43ee9086214849cc5550f15fcf
Liang Geng <geng.161@osu.edu>
2023-04-21 23:46:38 -0400

    Add sync test

--- examples/cpp/test/greeter_client.cc
+++ examples/cpp/test/greeter_client.cc
@@ -21,7 +21,7 @@
 #include <string>
 
 #include <grpcpp/grpcpp.h>
-
+#include "common.h"
 #ifdef BAZEL_BUILD
 #include "examples/protos/helloworld.grpc.pb.h"
 #else
@@ -42,10 +42,10 @@ class GreeterClient {
 
   // Assembles the client's payload, sends it and presents the response back
   // from the server.
-  std::string SayHello(const std::string& user) {
+  void SayHello(int idx, const std::string& msg) {
     // Data we are sending to the server.
     HelloRequest request;
-    request.set_name(user);
+    request.set_name(msg);
 
     // Container for the data we expect from the server.
     HelloReply reply;
@@ -59,11 +59,11 @@ class GreeterClient {
 
     // Act upon its status.
     if (status.ok()) {
-      return reply.message();
+      GPR_ASSERT(msg == reply.message());
+      std::cout << "Greeter " << idx << " received." << std::endl;
     } else {
-      std::cout << status.error_code() << ": " << status.error_message()
-                << std::endl;
-      return "RPC failed";
+      std::cout << "RPC failed" << std::endl;
+      exit(1);
     }
   }
 
@@ -72,37 +72,23 @@ class GreeterClient {
 };
 
 int main(int argc, char** argv) {
-  // Instantiate the client. It requires a channel, out of which the actual RPCs
-  // are created. This channel models a connection to an endpoint specified by
-  // the argument "--target=" which is the only expected argument.
-  // We indicate that the channel isn't authenticated (use of
-  // InsecureChannelCredentials()).
-  std::string target_str;
-  std::string arg_str("--target");
+  std::string port = "50051";
   if (argc > 1) {
-    std::string arg_val = argv[1];
-    size_t start_pos = arg_val.find(arg_str);
-    if (start_pos != std::string::npos) {
-      start_pos += arg_str.size();
-      if (arg_val[start_pos] == '=') {
-        target_str = arg_val.substr(start_pos + 1);
-      } else {
-        std::cout << "The only correct argument syntax is --target="
-                  << std::endl;
-        return 0;
-      }
-    } else {
-      std::cout << "The only acceptable argument is --target=" << std::endl;
-      return 0;
-    }
-  } else {
-    target_str = "localhost:50051";
+    port = std::string(argv[1]);
   }
+  auto server_address = "localhost:" + port;
+  printf("Connecting to %s\n", server_address.c_str());
+
   GreeterClient greeter(
-      grpc::CreateChannel(target_str, grpc::InsecureChannelCredentials()));
-  std::string user("world");
-  std::string reply = greeter.SayHello(user);
-  std::cout << "Greeter received: " << reply << std::endl;
+      grpc::CreateChannel(server_address, grpc::InsecureChannelCredentials()));
+  for (int i = 0; i < TOTAL_N_REQS; i++) {
+    auto msg_size = get_msg_size(min_msg_size, max_msg_size);
+    auto msg = gen_random_msg(msg_size);
+
+    greeter.SayHello(i, msg);  // The actual RPC call!
+  }
+
+  printf("PID: %d, Client Exit\n", getpid());
 
   return 0;
 }
--- examples/cpp/test/test.sh
+++ examples/cpp/test/test.sh
@@ -4,6 +4,19 @@ set -e
 PORT=$(shuf -i 2000-65000 -n 1)
 NP=4
 
+function start_server() {
+  TEST_BUILD_ROOT=$1
+  "$TEST_BUILD_ROOT"/greeter_server $PORT |& tee server.log &
+  SERVER_PID=$!
+}
+
+function start_client() {
+  TEST_BUILD_ROOT=$1
+  mpirun --bind-to none -n $NP -output-filename client_log "$TEST_BUILD_ROOT"/greeter_client $PORT
+  echo "Killing $SERVER_PID"
+  kill -9 $SERVER_PID
+}
+
 function start_async_server() {
   TEST_BUILD_ROOT=$1
   "$TEST_BUILD_ROOT"/greeter_async_server $PORT |& tee server.log &
@@ -30,9 +43,9 @@ function cleanup() {
   fi
 }
 
-if [[ $# -eq 0 ]] ; then
-    echo 'No args'
-    exit 1
+if [[ $# -eq 0 ]]; then
+  echo 'No args'
+  exit 1
 fi
 
 for i in "$@"; do
@@ -43,12 +56,19 @@ for i in "$@"; do
     ;;
   --test-client=*)
     prefix="${i#*=}"
+    start_server "$prefix"
+    start_client "$prefix"
+    cleanup
+    shift
+    ;;
+  --test-async-client=*)
+    prefix="${i#*=}"
     start_async_server "$prefix"
     start_async_client "$prefix"
     cleanup
     shift
     ;;
-  --test-client2=*)
+  --test-async-client2=*)
     prefix="${i#*=}"
     start_async_server "$prefix"
     start_async_client2 "$prefix"
