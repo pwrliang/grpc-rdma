4501a5596ef20d8c32c2fc08e49dc7298c60170d
Liang Geng <geng.161@osu.edu>
2023-05-05 15:04:23 -0400

    Minor changes

--- examples/cpp/grpc_microbenchmark/greeter_async_server2.cc
+++ examples/cpp/grpc_microbenchmark/greeter_async_server2.cc
@@ -53,20 +53,6 @@ using helloworld::RecvBufResponse;
 
 void* tag(int i) { return reinterpret_cast<void*>(i); }
 
-int bind_thread_to_core(int core_id) {
-  int num_cores = sysconf(_SC_NPROCESSORS_ONLN);
-  if (core_id < 0 || core_id >= num_cores) {
-    return EINVAL;
-  }
-
-  cpu_set_t cpuset;
-  CPU_ZERO(&cpuset);
-  CPU_SET(core_id, &cpuset);
-
-  pthread_t current_thread = pthread_self();
-  return pthread_setaffinity_np(current_thread, sizeof(cpu_set_t), &cpuset);
-}
-
 class ServerImpl final {
  public:
   ~ServerImpl() {
