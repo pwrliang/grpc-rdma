29f2c22f7c2edd77c5c59b414a10575f3259f5d3
Liang Geng <geng.161@osu.edu>
2024-02-10 15:27:31 -0500

    Fix

--- include/grpcpp/impl/codegen/call.h
+++ include/grpcpp/impl/codegen/call.h
@@ -21,21 +21,6 @@
 #include <grpc/impl/codegen/grpc_types.h>
 #include <grpcpp/impl/codegen/call_hook.h>
 
-enum platform_t {
-  IOMGR_TCP,
-  IOMGR_RDMA_BP,
-  IOMGR_RDMA_EVENT,
-  IOMGR_RDMA_BPEV
-};
-
-platform_t grpc_check_iomgr_platform();
-
-void* grpc_call_require_zerocopy_sendspace(grpc_call* call, size_t size);
-
-uint8_t* global_sendbuf_alloc(size_t size);
-
-bool global_sendbuf_free(uint8_t* buf);
-
 namespace grpc {
 class CompletionQueue;
 namespace experimental {
--- src/core/lib/iomgr/iomgr_internal.h
+++ src/core/lib/iomgr/iomgr_internal.h
@@ -20,7 +20,7 @@
 #define GRPC_CORE_LIB_IOMGR_IOMGR_INTERNAL_H
 
 #include <grpc/support/port_platform.h>
-//#include <grpcpp/impl/codegen/call.h>
+// #include <grpcpp/impl/codegen/call.h>
 
 #include <stdbool.h>
 
@@ -42,9 +42,9 @@ typedef struct grpc_iomgr_platform_vtable {
                                            grpc_error_handle error);
 } grpc_iomgr_platform_vtable;
 
-// enum platform_t {IOMGR_TCP, IOMGR_RDMA_BP, IOMGR_RDMA_EVENT};
+enum platform_t { IOMGR_TCP, IOMGR_RDMA_BP, IOMGR_RDMA_BPEV, IOMGR_RDMA_EVENT };
 
-// platform_t grpc_check_iomgr_platform();
+platform_t grpc_check_iomgr_platform();
 
 void grpc_iomgr_register_object(grpc_iomgr_object* obj, const char* name);
 void grpc_iomgr_unregister_object(grpc_iomgr_object* obj);
