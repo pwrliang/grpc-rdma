7a78cf4d1d171816232ae33506619d4981e2d6e8
Liang Geng <geng.161@osu.edu>
2024-02-26 14:55:51 -0500

    Skip TCP and Event for now

--- .github/workflows/run_all_variants.sh
+++ .github/workflows/run_all_variants.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-for type in TCP RDMA_BP RDMA_EVENT RDMA_BPEV; do
+#for type in TCP RDMA_BP RDMA_EVENT RDMA_BPEV; do
+for type in RDMA_BP RDMA_BPEV; do
   echo "Evaluating $type $1 $2"
   GRPC_PLATFORM_TYPE=$type $(realpath "$1") "$2"
 done
